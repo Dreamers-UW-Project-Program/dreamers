2716e700140dd1527a42033208e69a87e0426e71
wuming233 <stevenchenzijian@icloud.com>
2023-03-25 18:29:11 -0400

    Added endpoint for user avatar

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -9,13 +9,14 @@ type Data = {
     email?: string,
     passHash?: string,
     username?: string,
+    avatar?: string,
     message?: string
 }
 
-async function userListHandler(email: string, passHash: string, username: string) {
+async function userListHandler(email: string, passHash: string, username: string, avatar: string) {
     const userListRef = ref(db, '/userList');
     const newUserRef = await push(userListRef);
-    await set(newUserRef, { email, passHash, username });
+    await set(newUserRef, { email, passHash, username, avatar });
     return newUserRef.key;
 }
 
@@ -36,7 +37,7 @@ export default async function registerHandler(
 
 
     try{
-        const { email, password, username } = req.body;
+        const { email, password, username, avatar } = req.body;
         const encoded = encodeEmail(email);
 
         const isDuplicate = await duplicateEmail(encoded);
@@ -48,10 +49,10 @@ export default async function registerHandler(
         const saltOrRounds = 10;
         const passHash = await bcrypt.hash(password, saltOrRounds);
 
-        const key = await userListHandler(encoded, passHash, username);
+        const key = await userListHandler(encoded, passHash, username, avatar);
         await userIdListHandler(encoded, key || "INVALID");
 
-        res.status(201).json({ email: encoded, passHash, username });
+        res.status(201).json({ email: encoded, passHash, username, avatar });
     } catch {
         res.status(400).send({ message: 'Invalid body parameters'});
     }    
