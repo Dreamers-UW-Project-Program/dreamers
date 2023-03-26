87e1982eb9fa92f434349b1ea9e8bc00105168f6
Daniel Xu <danielxu004@gmail.com>
2023-03-26 19:44:40 -0400

    Update margin top on DreamFeed

--- components/DreamFeed/DreamFeed.tsx
+++ components/DreamFeed/DreamFeed.tsx
@@ -19,7 +19,7 @@ const DreamFeed = () => {
     }, [])
 
     return (
-        <div className="rounded-[2rem] mt-[4vw] w-[80%] h-[100vw] bg-transparent border-2 border-white z-30">
+        <div className="rounded-[2rem] mt-[2vw] w-[80%] h-[100vw] bg-transparent border-2 border-white z-30">
             <p className="font-semibold text-white font-serif text-2xl m-7 border-b-[1vw]">Your Friends Dreamed of......</p>
             <div className="flex flex-col gap-[1vw]">
                 {Object.keys(posts).map(id => {
