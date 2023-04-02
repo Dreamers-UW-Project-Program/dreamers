f70081d33bd5198ad3f3186b5651c709225df070
wuming233 <stevenchenzijian@icloud.com>
2023-04-02 14:18:13 -0400

    Fetches only 5 posts this time

--- components/DreamFeed/DreamFeed.tsx
+++ components/DreamFeed/DreamFeed.tsx
@@ -1,54 +1,80 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import FeedPost from "./FeedPost";
 import { Post } from "@customTypes/globals";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from 'react-infinite-scroller';
 
 const DreamFeed = () => {
-    const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState<{[id: string]: Post}>({});
     // const [lastKey, setLastKey] = useState("");
     const [hasMore, setHasMore] = useState(true);
+    let loading = false;
+    // let lastKey = "";
 
-    let lastKey = "";
+    // const loadMore = async () => {
+    //     const fetchedPosts = await fetch(`/api/posts?startKey="${lastKey}"&num=5`, {
+    //         method: "GET",
+    //     }).then(response => {
+    //         if (response.status === 404) {
+    //             return null;
+    //         }
+    //         return response.json();
+    //     });
 
-    const loadMore = async () => {
-        const fetchedPosts = await fetch(`/api/posts?startKey="${lastKey}"&num=5`, {
-            method: "GET",
-        }).then(response => {
-            if (response.status === 404) {
-                return null;
-            }
-            return response.json();
-        });
-
-        if (Object.keys(fetchedPosts).length <= 1) {
-            setHasMore(false);
-            console.log("Page False");
-            return;
-        }
+    //     if (Object.keys(fetchedPosts).length <= 1) {
+    //         setHasMore(false);
+    //         console.log("Page False");
+    //         return;
+    //     }
         
-        setPosts(prevPosts => ({ ...prevPosts, ...fetchedPosts }));
-        console.log("Loaded Page");
+    //     setPosts(prevPosts => ({ ...prevPosts, ...fetchedPosts }));
+    //     console.log("Loaded Page");
 
 
-        console.log("keys", Object.keys(fetchedPosts));
-        // console.log("last key:", Object.keys(fetchedPosts)[Object.keys(fetchedPosts).length - 1])
-        const key = String(Object.keys(fetchedPosts)[Object.keys(fetchedPosts).length - 1]);
-        console.log("key:", key);
-        // await setLastKey("no longer empty string mf");
-        lastKey = key;
-        console.log("lastKey:", lastKey);
-        console.log(Object.keys(posts).length);
-    };
+    //     console.log("keys", Object.keys(fetchedPosts));
+    //     // console.log("last key:", Object.keys(fetchedPosts)[Object.keys(fetchedPosts).length - 1])
+    //     const key = String(Object.keys(fetchedPosts)[Object.keys(fetchedPosts).length - 1]);
+    //     console.log("key:", key);
+    //     // await setLastKey("no longer empty string mf");
+    //     lastKey = key;
+    //     console.log("lastKey:", lastKey);
+    //     console.log(Object.keys(posts).length);
+    // };
+
+    const loadMore = () => {
+        loading = !loading;
+    }
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const fetchedPosts = await fetch(`/api/posts?startKey=""&num=5`, {
+                method: "GET",
+            }).then(response => {
+                if (response.status === 404) {
+                    return null;
+                }
+                return response.json();
+            });
+    
+            if (fetchedPosts) {
+                setPosts(prevPosts => ({ ...prevPosts, ...fetchedPosts }));
+            }
+    
+            console.log(posts);
+    
+            setHasMore(false);
+            console.log("page loaded. hasmore:", hasMore);
+        }
+
+        getPosts();
+    }, []);
 
     return (
         <div className="flex flex-col white-box-shadow rounded-[2rem] mt-[2vw] w-[80%] bg-transparent border-2 border-white z-30">
             <p className="font-semibold text-white font-montserratBold text-4xl m-7 border-b-[1vw]">Your Friends Dreamed of......</p>
             <InfiniteScroll
-                dataLength={Object.keys(posts).length}
-                next={loadMore}
-                // height={400}
-                // scrollThreshold={0.2}
-                hasMore={hasMore}
+                pageStart={0}
+                loadMore={loadMore}
+                hasMore={false}
                 loader={<div className="loader" key={0}>Loading ...</div>}
             >
                 <div className="flex flex-col gap-[1vw]">
@@ -62,8 +88,8 @@ const DreamFeed = () => {
                                     title={posts[id]["title"]}
                                     authorID={posts[id]["authorID"]}
                                     date={date}
-                                    // thumbnail={posts[id]["thumbnail"]}
-                                    thumbnail={"https://cdn.discordapp.com/attachments/772859425261748255/1086762019690139729/photo-1524024973431-2ad916746881.jpg"}
+                                    thumbnail={posts[id]["thumbnail"]}
+                                    // thumbnail={"https://cdn.discordapp.com/attachments/772859425261748255/1086762019690139729/photo-1524024973431-2ad916746881.jpg"}
                                     comments={posts[id]["comments"] ?? {}}
                                     likes={posts[id]["likes"] ?? {}}
                                 />
