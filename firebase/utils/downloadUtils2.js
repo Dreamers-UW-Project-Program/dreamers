81f12296a57af961590486c6974edc2ec6a35de9
Fan-Yang <fan.yang284@gmail.com>
2023-04-02 21:01:41 -0400

    move profile images to another firebase instance

---
+++ firebase/utils/downloadUtils2.js
@@ -0,0 +1,22 @@
+import { storage2 } from "../firebase";
+import { getDownloadURL, ref } from "firebase/storage";
+
+/* 
+*  returns a download url, which can be used as described in 
+*  https://firebase.google.com/docs/storage/web/download-files#download_data_via_url
+*/ 
+export const getDownloadUrlFromPath = async (path) => {
+	const pathReference = ref(storage2, path);
+	const url = await getDownloadURL(pathReference);
+	return url;
+}
+
+export const getAllFilesFromFolderUrl = async (folderUrl) => {
+	const folderRef = ref(storage2, folderUrl);
+	// Find all the prefixes, items and return them
+	listAll(folderRef).then((res) => {
+			return res;
+		}).catch((error) => {
+			console.log(error.code);
+		});
+}
--- firebase/utils/uploadUtils.js
+++ firebase/utils/uploadUtils.js
@@ -1,5 +1,5 @@
 import { ref, uploadString, listAll } from "firebase/storage";
-import { storage } from "../firebase";
+import { storage, storage2 } from "../firebase";
 
 export const toDataURL = async (url) => {
 	let blob = await fetch(url).then(r => r.blob());
@@ -12,7 +12,7 @@ export const toDataURL = async (url) => {
 }
 
 export const uploadProfileImageFromUrl = async (url, imageName) => {
-	const profileImageRef = ref(storage, `user-profile-images/${imageName}`);
+	const profileImageRef = ref(storage2, `user-profile-images/${imageName}`);
 	const dataUrl = await toDataURL(url);	
 	console.log(dataUrl);
 	await uploadString(profileImageRef, dataUrl, 'data_url').then((snapshot) => {
@@ -21,35 +21,35 @@ export const uploadProfileImageFromUrl = async (url, imageName) => {
 }
 
 export const uploadProfileImageFromFile = async (file, imageName) => {
-	const profileImageRef = ref(storage, `user-profile-images/${imageName}`);
+	const profileImageRef = ref(storage2, `user-profile-images/${imageName}`);
 	uploadBytes(profileImageRef, file).then((snapshot)=> {
 		console.log('Uploaded a blob or file!');
 	})
 }
 
 export const uploadProfileImageFromBase64 = async (base64, imageName) => {
-	const profileImageRef = ref(storage,  `user-profile-images/${imageName}`);
+	const profileImageRef = ref(storage2,  `user-profile-images/${imageName}`);
 	await uploadString(profileImageRef, base64, 'base64').then((snapshot) => {
 		console.log('Uploaded a base64 string!');
 	});
 }
 
 export const uploadProfileImageFromBase64url = async (base64url, imageName) => {
-	const profileImageRef = ref(storage,  `user-profile-images/${imageName}`);
+	const profileImageRef = ref(storage2,  `user-profile-images/${imageName}`);
 	await uploadString(profileImageRef, base64url, 'base64url').then((snapshot) => {
 		console.log('Uploaded a base64 string!');
 	});
 }
 
 export const uploadProfileImageFromDataUrl = async (dataUrl, imageName) => {
-	const profileImageRef = ref(storage,  `user-profile-images/${imageName}`);
+	const profileImageRef = ref(storage2,  `user-profile-images/${imageName}`);
 	await uploadString(profileImageRef, dataUrl, 'data_url').then((snapshot) => {
 		console.log('Uploaded a base64 string!');
 	});
 }
 
 export const uploadPostThumbnailFromBase64 = async (base64, imageName) => {
-	const profileImageRef = ref(storage,  `post-thumbnails/${imageName}`);
+	const profileImageRef = ref(storage2,  `post-thumbnails/${imageName}`);
 	await uploadString(profileImageRef, base64, 'base64').then((snapshot) => {
 		console.log('Uploaded a base64 string!');
 	});
